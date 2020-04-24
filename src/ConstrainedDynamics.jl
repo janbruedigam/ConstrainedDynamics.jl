c8020fa91c1b6f570e320346c2faa2d2f5c67dfc
janbruedigam <jan.bruedigam@t-online.de>
2020-04-24 18:00:23 +0200

    Files generated by PkgTemplates

---
+++ src/ConstrainedDynamics.jl
@@ -0,0 +1,5 @@
+module ConstrainedDynamics
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ConstrainedDynamics
+using Test
+
+@testset "ConstrainedDynamics.jl" begin
+    # Write your own tests here.
+end
