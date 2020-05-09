a496f73f042b8d50685ebfe089cfe001c8f31956
janbruedigam <jan.bruedigam@t-online.de>
2020-05-09 18:20:11 +0200

    Add test tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using ConstrainedDynamics
+using StaticArrays
 using Test
 
+
 @testset "ConstrainedDynamics.jl" begin
-    # Write your own tests here.
+    @test ConstrainedDynamics.skew([1;2;3]) == SMatrix{3,3,Int64,9}(0, 3, -2, -3, 0, 1, 2, -1, 0)
+    @test ConstrainedDynamics.skew([1;2;4]) == SMatrix{3,3,Int64,9}(0, 4, -2, -4, 0, 1, 2, -1, 0)
 end
