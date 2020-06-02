e7e3d32306c8648b97482a27a2e31b145412b628
janbruedigam <jan.bruedigam@t-online.de>
2020-06-02 17:39:59 +0200

    Move allocation tests to benchmark tests.

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,10 +1,40 @@
 using BenchmarkTools
 
 SUITE = BenchmarkGroup()
-SUITE["sum"] = @benchmarkable sum($(randn(10_000)))
-SUITE["sum2"] = @benchmarkable sum($(randn(10_000)))
 
-include("../test/examples/atlas.jl")
-steps = Base.OneTo(1000)
-storage = Storage{Float64}(steps,length(mech.bodies))
-SUITE["atlastest"] = @benchmarkable simulate!($mech, $steps, $storage) samples=1
+files = [
+    "atlas"
+    "axes"
+    "chain_in_chain"
+    "dice_nofriction"
+    "dice_tiltedplane"
+    "dice"
+    "disconnected_bodies"
+    "doublependulum_3d"
+    "doublependulum_urdf"
+    "football"
+    "inverted_pyramid_plane"
+    "joint_force"
+    "joint_torque"
+    "n_fourbars"
+    "n_pendulum"
+    "nutation"
+    "pendulum_forced"
+    "pendulum"
+    "planar_example"
+    "scissor_lift"
+    "slider_crank"
+    "slider_crank3d"
+    "wheel"
+]
+
+for i=1:length(files)-1
+    include("../test/examples/"*files[i]*".jl")
+    steps = Base.OneTo(1000)
+    storage = Storage{Float64}(steps,length(mech.bodies))
+    if files[i]=="joint_force" || files[i]=="pendulum_forced" || files[i]=="football" || files[i]=="nutation"
+        SUITE[files[i]] = @benchmarkable simulate!($mech, $steps, $storage, $control!) samples=1
+    else
+        SUITE[files[i]] = @benchmarkable simulate!($mech, $steps, $storage) samples=1
+    end
+end
