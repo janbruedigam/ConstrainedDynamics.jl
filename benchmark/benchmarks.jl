cc81ee01bd7c4dbb8a35527d6c85b3dcd69a95dd
janbruedigam <jan.bruedigam@t-online.de>
2020-06-02 16:32:53 +0200

    Update path to benchmark examples.

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -4,7 +4,7 @@ SUITE = BenchmarkGroup()
 SUITE["sum"] = @benchmarkable sum($(randn(10_000)))
 SUITE["sum2"] = @benchmarkable sum($(randn(10_000)))
 
-include("examples/atlas.jl")
+include("../examples/atlas.jl")
 steps = Base.OneTo(1000)
 storage = Storage{Float64}(steps,length(mech.bodies))
 SUITE["atlastest"] = @benchmarkable simulate!($mech, $steps, $storage) samples=1
