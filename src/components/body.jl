c01e0c46ac287059130284d746a8b094934c3270
janbruedigam <jan.bruedigam@t-online.de>
2020-06-14 18:14:33 +0200

    Move linearization and fix q linear dynamics

--- src/components/body.jl
+++ src/components/body.jl
@@ -63,16 +63,55 @@ Base.length(::Body) = 6
 
 @inline getM(body::Body{T}) where T = [[SMatrix{3,3,T,9}(I*body.m);@SMatrix zeros(T,3,3)] [@SMatrix zeros(T,3,3);body.J]]
 
-@inline function ∂zp1∂z(mechanism, body::Body{T}, xc, vc, Fk, qc, ωc, τk, Δt) where T
-    stateold = settempvars!(body, xc, vc, Fk, qc, ωc, τk, (@SVector zeros(T,6)))
 
-    discretizestate!(mechanism)
-    foreach(setsolution!, mechanism.bodies)
-    newton!(mechanism)
+@inline function ∂dyn∂vel(body::Body{T}, Δt) where T
+    J = body.J
+    ω2 = body.state.ωsol[2]
+    sq = sqrt(4 / Δt^2 - ω2' * ω2)
 
-    A, B = ∂zp1∂z(mechanism, body::Body{T}, Δt)
-    
-    body.state = stateold
+    dynT = SMatrix{3,3,T,9}(body.m / Δt * I)
+    dynR = skewplusdiag(ω2, sq) * J - J * ω2 * (ω2' / sq) - skew(J * ω2)
 
-    return A, B
+    Z = @SMatrix zeros(T, 3, 3)
+
+    return [[dynT; Z] [Z; dynR]]
+end
+
+@inline function ∂zp1∂z(mechanism, body::Body{T}, Δt) where T
+    state = body.state
+    Z = @SMatrix zeros(T,3,3)
+    Z2 = [Z Z]
+    Z2t = Z2'
+    E = SMatrix{3,3,T,9}(I)
+
+    # Position
+    AposT = [E E*Δt]
+    BposT = Z
+
+        # This calculates the ϵ for q⊗Δq = q⊗(1 ϵᵀ)ᵀ
+    AposR = VLᵀmat(state.qsol[2]) * [Rmat(ωbar(state.ωc, Δt))*LVᵀmat(state.qc) Lmat(state.qc)*derivωbar(state.ωc, Δt)]
+    BposR = Z
+
+    # Velocity
+    AvelT = [Z E]
+    BvelT = E*Δt/body.m
+
+    J = body.J
+    ω1 = state.ωc
+    ω2 = state.ωsol[2]
+    sq1 = sqrt(4 / Δt^2 - ω1' * ω1)
+    sq2 = sqrt(4 / Δt^2 - ω2' * ω2)
+
+    ω1func = skewplusdiag(-ω1, sq1) * J - J * ω1 * (ω1' / sq1) + skew(J * ω1)
+    ω2func = skewplusdiag(ω2, sq2) * J - J * ω2 * (ω2' / sq2) - skew(J * ω2)
+    AvelR = [Z ω2func\ω1func]
+    BvelR = 2*inv(ω2func)
+
+
+    AT = [[AposT;AvelT] [Z2;Z2]]
+    AR = [[Z2;Z2] [AposR;AvelR]]
+    BT = [[BposT;BvelT] Z2t]
+    BR = [Z2t [BposR;BvelR]]
+
+    return [AT;AR], [BT;BR]
 end
