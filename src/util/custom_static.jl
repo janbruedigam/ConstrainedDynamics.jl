9c8c832b499474b07586e2ef6e07cec78a820b5c
janbruedigam <jan.bruedigam@t-online.de>
2020-06-15 23:46:25 +0200

    Fix cat allocations

--- src/util/custom_static.jl
+++ src/util/custom_static.jl
@@ -13,8 +13,17 @@ Base.:*(u::LinearAlgebra.AdjointAbsVec, v::SVector{1,T}) where T = u * v[1]
 Base.:*(u::AbstractVector, v::SVector{1,T}) where T = u * v[1]
 
 
+Base.hcat(E::UniformScaling, v::SVector{N,T}) where {T, N} = hcat(SMatrix{N,N,T,N*N}(E), v)
+Base.hcat(v::SVector{N,T}, E::UniformScaling) where {T, N} = hcat(v, SMatrix{N,N,T,N*N}(E))
+Base.hcat(E::UniformScaling, v::Adjoint{T,SVector{N,T}}) where {T, N} = @error "not implemented" # shcat(E.λ, v)
+Base.hcat(v::Adjoint{T,SVector{N,T}}, E::UniformScaling) where {T, N} = @error "not implemented" # shcat(v, E.λ)
 Base.hcat(E::UniformScaling, A::SMatrix{N1,N2,T,N1N2}) where {T, N1, N2, N1N2} = hcat(SMatrix{N1,N1,T,N1*N1}(E), A)
 Base.hcat(A::SMatrix{N1,N2,T,N1N2}, E::UniformScaling) where {T, N1, N2, N1N2} = hcat(A, SMatrix{N1,N1,T,N1*N1}(E))
+
+Base.vcat(E::UniformScaling, v::SVector{N,T}) where {T, N} = svcat(E.λ, v)
+Base.vcat(v::SVector{N,T}, E::UniformScaling) where {T, N} = svcat(v, E.λ)
+Base.vcat(E::UniformScaling, v::Adjoint{T,SVector{N,T}}) where {T, N} = vcat(SMatrix{N,N,T,N*N}(E), v)
+Base.vcat(v::Adjoint{T,SVector{N,T}}, E::UniformScaling) where {T, N} = vcat(v, SMatrix{N,N,T,N*N}(E))
 Base.vcat(E::UniformScaling, A::SMatrix{N1,N2,T,N1N2}) where {T, N1, N2, N1N2} = vcat(SMatrix{N2,N2,T,N2*N2}(E), A)
 Base.vcat(A::SMatrix{N1,N2,T,N1N2}, E::UniformScaling) where {T, N1, N2, N1N2} = vcat(A, SMatrix{N2,N2,T,N2*N2}(E))
 
