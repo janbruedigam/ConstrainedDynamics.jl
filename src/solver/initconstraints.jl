60a48b88930da8ee5dd249b2c4ca713cc8056173
zeinebBC <zeineb.ben-chaaben@tum.de>
2020-10-14 10:01:23 +0100

    eliminate fixed bodies

--- src/solver/initconstraints.jl
+++ src/solver/initconstraints.jl
@@ -1,3 +1,14 @@
+
+
+function get_free_bodies(mechanism::Mechanism{T},fixedbodies) where T
+    freebodies = Body{T}[]
+    for (j,body) in enumerate(mechanism.bodies)
+        if !(body.id in fixedbodies)
+        push!(freebodies,body)
+        end  
+    end  
+    return UnitDict(freebodies) 
+end
 function gc(mechanism::Mechanism{T}) where T
     rangeDict = Dict{Int64,UnitRange}()
     ind1 = 1
@@ -22,7 +33,7 @@ function gc(mechanism::Mechanism{T}) where T
 end   
 
 # Derivatives
-function ∂g∂posc(mechanism::Mechanism{T,N,Nb}) where {T,N,Nb}
+function ∂g∂posc(mechanism::Mechanism{T,N,Nb},fixedbodies) where {T,N,Nb}
     rangeDict = Dict{Int64,UnitRange}()
     ind1 = 1
     ind2 = 0
@@ -34,12 +45,12 @@ function ∂g∂posc(mechanism::Mechanism{T,N,Nb}) where {T,N,Nb}
 
         ind1 = ind2+1
     end
-
-    G = zeros(ind2,Nb*7)
+    Nfb=length(fixedbodies)
+    G = zeros(ind2,(Nb-Nfb)*7)
     
     for (i,eqc) in enumerate(mechanism.eqconstraints)
-        for (j,body) in enumerate(mechanism.bodies)
-            G[rangeDict[i],offsetrange(j,7)] = ∂g∂posc(mechanism, eqc, body.id)
+        for (j,body) in enumerate(get_free_bodies(mechanism,fixedbodies))
+             G[rangeDict[i],offsetrange(j,7)] = ∂g∂posc(mechanism, eqc, body.id)
         end
     end
         
@@ -47,48 +58,51 @@ function ∂g∂posc(mechanism::Mechanism{T,N,Nb}) where {T,N,Nb}
 end
 
 
-function constraintstep!(mechanism::Mechanism{T,N,Nb}) where {T,N,Nb}
-    bodies = mechanism.bodies
-
+function constraintstep!(mechanism::Mechanism{T,N,Nb},fixedbodies) where {T,N,Nb}
+    
     gval=gc(mechanism)
-    pinv∂g∂posc=pinv(∂g∂posc(mechanism))
+    pinv∂g∂posc=pinv(∂g∂posc(mechanism,fixedbodies))
     stepvec = -pinv∂g∂posc*gval
 
-    for (i,body) in enumerate(mechanism.bodies)
-        body.state.vsol[1] = stepvec[offsetrange(i, 3, 7, 1)]
+    for (i,body) in enumerate(get_free_bodies(mechanism,fixedbodies))
 
-        # Limit quaternion step to feasible length
-        range = offsetrange(i, 3, 7, 2)
-        Δstemp = VLᵀmat(bodies[i].state.qk[1]) * stepvec[first(range):(last(range)+1)]
-        if norm(Δstemp) > 1
-            Δstemp = Δstemp/norm(Δstemp)
-        end
-        body.state.ωsol[1] = Δstemp
+            body.state.vsol[1] = stepvec[offsetrange(i, 3, 7, 1)]
+            # Limit quaternion step to feasible length
+            range = offsetrange(i, 3, 7, 2)
+            Δstemp = VLᵀmat(body.state.qk[1]) * stepvec[first(range):(last(range)+1)]
+            if norm(Δstemp) > 1
+                Δstemp = Δstemp/norm(Δstemp)
+            end
+            body.state.ωsol[1] = Δstemp
+        
     end
 
     return
 end
 
-function initializeConstraints!(mechanism::Mechanism{T,N,Nb,Ne}; ε = 1e-5, newtonIter = 100, lineIter = 10) where {T,N,Nb,Ne}
-    bodies = mechanism.bodies
-
+function initializeConstraints!(mechanism::Mechanism{T,N,Nb,Ne},fixedbodies; ε = 1e-5, newtonIter = 100, lineIter = 10) where {T,N,Nb,Ne}
+    
     norm0 = norm(gc(mechanism))
     norm1 = norm0
     for n = Base.OneTo(newtonIter)
-        for body in bodies  
+
+        for body in get_free_bodies(mechanism,fixedbodies)  
             body.state.xk[1] = body.state.xc
             body.state.qk[1] = body.state.qc
         end
 
-        constraintstep!(mechanism) 
+        constraintstep!(mechanism,fixedbodies) 
+    
+        for j = Base.OneTo(lineIter)
 
-        for j = Base.OneTo(newtonIter)
-            for body in bodies
+            for body in get_free_bodies(mechanism,fixedbodies)
+                
                 body.state.xc = body.state.xk[1] + body.state.vsol[1]/(2^(j-1))
+                
                 w = sqrt(1-norm(body.state.ωsol[1]/(2^(j-1)))^2)
                 body.state.qc = body.state.qk[1] * UnitQuaternion(w,body.state.ωsol[1]/(2^(j-1))...,false)
             end
-
+            
             norm1 = norm(gc(mechanism))
 
             if norm1 < norm0 
